906da7d9d4464302e41d075180a204afd6c8c179
rithin45 <127735094+rithin45@users.noreply.github.com>
2024-02-17 11:59:16 +0530

    26

--- src/utils/Addpro.jsx
+++ src/utils/Addpro.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navsidebar from './Navbar/Navsidebar'
+import { TableBody, TableCell, TableRow } from '@mui/material'
+import{Buffer} from 'buffer';
+import EditIcon from '@mui/icons-material/Edit';
+import axios from 'axios';
+
 
 const Addpro = () => {
+  var [category, setCategory] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3005/view")
+      .then(response => {
+        setCategory(response.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+  const updateValues = (row) => {
+    setSelected(row)
+    setUpdate(true)
+  }
+
   return (
     <div>
         <Navsidebar/>
      <div>
-     Added Products
+     {/* Added Products */}
+     <TableBody>
+            {category.map((row, pos) => {
+
+              return (
+                <TableRow
+                  key={pos}>
+                  
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.offer_price}</TableCell>
+                  <TableCell>{row.MRP}</TableCell>
+                  <TableCell>{row.category}</TableCell>
+                  <TableCell><EditIcon onClick={() => updateValues(row)} /></TableCell>
+                   {/* <TableCell>
+                       <img src={`data:image/jpeg;base64,${Buffer.from(row.image1.data).toString('base64')}`}
+                        width="50" height="50" alt="Error"/></TableCell>  */}
+                        <TableCell>
+                          <img src={`data:image/jpeg;base64,${Buffer.from(row.image1.data).toString('base64')}`}width="50" height="50" alt='error'/>
+                        </TableCell>
+
+                </TableRow>
+
+              )
+            })}
+          </TableBody>
 
      </div>
     </div>
--- src/utils/Homeu.jsx
+++ src/utils/Homeu.jsx
@@ -12,8 +12,8 @@ const Homeu = () => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true, // Enable automatic sliding
-        autoplaySpeed: 3000 // Set the interval between slides (in milliseconds)
+        autoplay: true,
+        autoplaySpeed: 3000 
       };
   return (
     <div>
